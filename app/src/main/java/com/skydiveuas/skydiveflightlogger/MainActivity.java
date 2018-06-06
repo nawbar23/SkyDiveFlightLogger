a432d8415e21822cf424de64d4737959570bff3e
Bartosz Nawrot <nawbar23@gmail.com>
2018-06-06 18:20:39 +0200

    Add logger logic

--- app/src/main/java/com/skydiveuas/skydiveflightlogger/MainActivity.java
+++ app/src/main/java/com/skydiveuas/skydiveflightlogger/MainActivity.java
@@ -2,11 +2,12 @@ package com.skydiveuas.skydiveflightlogger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.skydiveuas.skydiveflightlogger.logger.Logger;
 import com.skydiveuas.skydiveflightlogger.usb.UsbOtgPort;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Logger.Listener {
     private static final String DEBUG_TAG = MainActivity.class.getSimpleName();
 
     private UsbOtgPort usb;
@@ -18,11 +19,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logger = new Logger();
+        logger = new Logger(this);
 
         usb = new UsbOtgPort(this);
         usb.setListener(logger);
 
         usb.connect();
+    }
+
+    @Override
+    public void onStarted() {
+        Log.e(DEBUG_TAG, "onStarted");
+    }
+
+    @Override
+    public void onEnded() {
+        Log.e(DEBUG_TAG, "onEnded");
+    }
+
+    @Override
+    public void onError(String message) {
+        Log.e(DEBUG_TAG, "onError: " + message);
+    }
+
+    @Override
+    public void onStatusUpdate(long time, double freq) {
+        Log.e(DEBUG_TAG, "onStatusUpdate: time: " + time + " ,freq: " + freq);
     }
 }
--- app/src/main/java/com/skydiveuas/skydiveflightlogger/logger/Logger.java
+++ app/src/main/java/com/skydiveuas/skydiveflightlogger/logger/Logger.java
@@ -1,10 +1,14 @@
 package com.skydiveuas.skydiveflightlogger.logger;
 
+import android.util.Log;
+
 import com.skydive.java.CommInterface;
 import com.skydive.java.data.CalibrationSettings;
 import com.skydive.java.data.ControlSettings;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Bartosz Nawrot on 2018-06-06.
@@ -13,44 +17,139 @@ import java.io.IOException;
 public class Logger implements CommInterface.CommInterfaceListener, Parser.Listener {
     private static final String DEBUG_TAG = Logger.class.getSimpleName();
 
+    private static final long MAX_INACTIVE_TIME = 3000; // [ms]
+    private static final long STATUS_UPDATE_INTERVAL = 200; // [ms]
+
+    private enum State { W4_CALIB, W4_CONTROL, LOGGING }
+
+    private State state;
+
+    private Listener listener;
     private Parser parser;
 
-    public Logger() {
+    private Timer timeoutTimer;
+    private Timer statusTimer;
+
+    private long startTime;
+    private long receivedCount;
+    private boolean received;
+
+    public Logger(Listener listener) {
+        this.listener = listener;
         this.parser = new Parser(this);
-    }
 
-    @Override
-    public void onReceived(ExtendedDebugData message) {
-
+        state = State.W4_CALIB;
     }
 
     @Override
     public void onReceived(CalibrationSettings message) {
-
+        if (state == State.W4_CALIB) {
+            // TODO safe to file
+            state = State.W4_CONTROL;
+        } else {
+            Log.e(DEBUG_TAG, "onReceived(CalibrationSettings) in bad state");
+        }
     }
 
     @Override
     public void onReceived(ControlSettings message) {
+        if (state == State.W4_CALIB) {
+            // TODO safe to file
+            startLog();
+        } else {
+            Log.e(DEBUG_TAG, "onReceived(ControlSettings) in bad state");
+        }
+    }
 
+    @Override
+    public void onReceived(ExtendedDebugData message) {
+        if (state == State.LOGGING) {
+            // TODO safe to file
+            received = true;
+        } else {
+            Log.e(DEBUG_TAG, "onReceived(ExtendedDebugData) in bad state");
+        }
     }
 
     @Override
     public void onConnected() {
-
+        Log.e(DEBUG_TAG, "onConnected");
     }
 
     @Override
     public void onDisconnected() {
-
+        endLog();
+        listener.onError("Board disconnected");
     }
 
     @Override
     public void onError(IOException e) {
-
+        endLog();
+        listener.onError(e.getMessage());
     }
 
     @Override
     public void onDataReceived(byte[] data, int dataSize) {
+        parser.parse(data, dataSize);
+    }
 
+    private TimerTask timeoutTask = new TimerTask() {
+        @Override
+        public void run() {
+            if (state == State.LOGGING && !received) {
+                endLog();
+            }
+            received = false;
+        }
+    };
+
+    private TimerTask statusTask = new TimerTask() {
+        @Override
+        public void run() {
+            long time = System.currentTimeMillis() - startTime;
+            if (time > 0) {
+                listener.onStatusUpdate(time, receivedCount / (time / 1000.0));
+            }
+        }
+    };
+
+    private void startLog() {
+        openFile();
+        startTime = System.currentTimeMillis();
+        receivedCount = 0;
+        timeoutTimer = new Timer();
+        timeoutTimer.scheduleAtFixedRate(timeoutTask, MAX_INACTIVE_TIME, MAX_INACTIVE_TIME);
+        statusTimer = new Timer();
+        statusTimer.scheduleAtFixedRate(statusTask, STATUS_UPDATE_INTERVAL, STATUS_UPDATE_INTERVAL);
+        listener.onStarted();
+        state = State.LOGGING;
+    }
+
+    private void endLog() {
+        closeFile();
+        timeoutTimer.cancel();
+        timeoutTimer = null;
+        statusTimer.cancel();
+        statusTimer = null;
+        listener.onEnded();
+        state = State.W4_CALIB;
+    }
+
+    private void openFile() {
+        // TODO open new files
+    }
+
+    private void closeFile() {
+        // TODO close opened files
+    }
+
+    public interface Listener {
+        void onStarted();
+
+        void onEnded();
+
+        void onError(String message);
+
+        void onStatusUpdate(long time, double freq);
     }
 }
